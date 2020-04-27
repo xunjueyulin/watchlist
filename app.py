df3868bec56f3d25fee12b514f6b054e9b86afbb
xunjueyulin <xunjueyulin@hotmail.com>
2020-04-27 15:03:26 +0800

    Add index page in PHICOMM N1

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+name = 'Xunjueyulin'
+movies = [
+        {'title':'My Neighbor Totoro','year': '1988'},
+        {'title':'Destiny:Kamakura Monogatari','year':'2017'},
+        {'title':'Thor:The Dark World','year':'2013'},
+        {'title':'Avengers:Age of Ultron','year':'2013'},
+        {'title':'Iron Man 2','year':'2010'},
+        {'title':'Captain America:Civil War','year':'2016'},
+        {'title':'Yip Man 4:The Finale','year':'2019'},
+        {'title':'Avengers:Endgame','year':'2019'},
+        {'title':'Aquaman','year':'2019'},
+        ]
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return u'欢迎来到我的 Watchlist!'
+def index():
+    return render_template('index.html', name=name, movies=movies)
